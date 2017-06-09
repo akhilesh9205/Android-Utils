b7cfa4f63c206805e28a77154d98029b40943157
Akhilesh Kumar <akhileshkumar9205@gmail.com>
2017-06-09 17:14:50 +0530

    Version check in method removed by using classes from support library

--- andoidutils/src/main/java/in/codeselect/andoidutils/ResUtil.java
+++ andoidutils/src/main/java/in/codeselect/andoidutils/ResUtil.java
@@ -2,10 +2,11 @@ package in.codeselect.andoidutils;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.StringRes;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.content.res.AppCompatResources;
 
 /**
  * Created by akhileshkumar on 9/6/17.
@@ -38,19 +39,11 @@ public class ResUtil {
     }
 
     private Drawable getDrawableRes(@DrawableRes int id) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return mContext.getDrawable(id);
-        } else {
-            return mContext.getResources().getDrawable(id);
-        }
+        return AppCompatResources.getDrawable(mContext, id);
     }
 
     private int getColorRes(@ColorRes int id){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return mContext.getColor(id);
-        } else {
-            return mContext.getResources().getColor(id);
-        }
+        return ContextCompat.getColor(mContext, id);
     }
 
     public static String getString(@StringRes int id) {
